de90b917916c27f71605d72ac36e86a153659bd3
MattoDev <mateofonseca.profesional@gmail.com>
2022-03-09 08:15:58 -0500

    refactoring

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import { TodoContext } from '../TodoContext';
-import './TodoForm.css';
+import React from "react";
 
-function TodoForm() {
-  const [newTodoValue, setNewTodoValue] = React.useState('');
-  const {
-    addTodo,
-    setOpenModal,
-  } = React.useContext(TodoContext);
-  
+import "./TodoForm.css";
+
+function TodoForm({ addTodo, setOpenModal }) {
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+
   const onChange = (event) => {
     setNewTodoValue(event.target.value);
   };
@@ -34,13 +30,10 @@ function TodoForm() {
           type="button"
           className="TodoForm-button TodoForm-button--cancel"
           onClick={onCancel}
-          >
+        >
           Cancelar
         </button>
-        <button
-          type="submit"
-          className="TodoForm-button TodoForm-button--add"
-        >
+        <button type="submit" className="TodoForm-button TodoForm-button--add">
           Añadir
         </button>
       </div>
